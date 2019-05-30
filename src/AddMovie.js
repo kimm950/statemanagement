887a67f97ceea15fd2bdc663b9cb06f6793d203a
kimm950 <hyunsung.kim@zeals.co.jp>
2019-05-30 14:56:44 +0900

    final

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,10 +1,10 @@
 import React, { useState, useContext } from 'react';
-import { MovieContext } from './MovieContext'
+import { MovieContext } from './MovieContext';
 
 const AddMovie = () => {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
-  const [movies, setMovies] = useContext(MovieContext);
+  const [movies, setMovies] = useContext(MovieContext)
 
   const updateName = (e) => {
     setName(e.target.value);
